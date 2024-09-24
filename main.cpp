58a9c8327b4f0042199fb40b9b3c8aa5a3e6c5f3
Humbert <122186863+xsabreusx@users.noreply.github.com>
2024-09-24 16:35:14 -0500

    Updated main file

--- main.cpp
+++ main.cpp
@@ -3,15 +3,12 @@
 #include <cmath>
 using namespace std;
 
-int main(){
-
-
-}
-
 class Shapes {
+public:
+virtual double getPerimeter() = 0;
+virtual double getArea() = 0;
 
-virtual double getPerimeter()=0;
-virtual double getArea()=0;
+private:
 
 };
 
@@ -19,21 +16,20 @@ class Circle: public Shapes {
 public:
 Circle(int radius){
 radius_ = radius;
-}
+};
 
 double getPerimeter() {
-    double Perimeter = 6.28*radius_; 
+    double Perimeter = 6.28 * radius_; //2pi(r)
     return Perimeter;   
 }
 
 double getArea() {
-    double Area = 3.14*(radius_^2);
+    double Area = 3.14*(pow(radius_,2)); //pi(r)^2
     return Area;
 }
 
 private:
 int radius_ = 0;
-
 
 };
 
@@ -45,12 +41,12 @@ RTriangle(int length, int width){
 }
 
 double getPerimeter(){
-    double Perimeter = length_ + width_ + (sqrt((length_^2) +(width_^2)));
+    double Perimeter = length_ + width_ + (sqrt((pow(length_,2)) +(pow(width_,2)))); //Pythagorean Theorem + length and width
     return Perimeter;
 }
 
 double getArea(){
-    double Area = .5*length_*width_;
+    double Area = .5 * length_ * width_; //(1/2)l*w
     return Area;
 }
 
@@ -68,12 +64,12 @@ Rectangle(int length, int width){
 }
 
 double getPerimeter(){
-    double Perimeter = (2*length_) + (2*width_);
+    double Perimeter = (2 * length_) + (2 * width_); //All sides added together
     return Perimeter;
 }
 
 double getArea(){
-    double Area = length_*width_;
+    double Area = length_ * width_; //length times width
     return Area;
 }
 
@@ -81,3 +77,21 @@ private:
 int length_ = 0;
 int width_ = 0;
 };
+
+
+int main(){
+
+Circle c1(1);
+RTriangle t1(3,4);
+Rectangle r1(1,2);
+
+assert(c1.getArea()== 3.14 );
+assert(c1.getPerimeter()== 6.28 );
+assert(t1.getArea()== 6);
+assert(t1.getPerimeter()== 12);
+assert(r1.getArea()== 2);
+assert(r1.getPerimeter()== 6);
+cout << "\nAll passed";
+
+}
+
