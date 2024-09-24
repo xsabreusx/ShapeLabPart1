25757154f047deec0b8924bf1f921866532863df
Humbert <122186863+xsabreusx@users.noreply.github.com>
2024-09-24 15:54:54 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <cmath>
 using namespace std;
 
 int main(){
@@ -9,8 +10,8 @@ int main(){
 
 class Shapes {
 
-virtual void getPerimeter()=0;
-virtual void getArea()=0;
+virtual double getPerimeter()=0;
+virtual double getArea()=0;
 
 };
 
@@ -18,6 +19,16 @@ class Circle: public Shapes {
 public:
 Circle(int radius){
 radius_ = radius;
+}
+
+double getPerimeter() {
+    double Perimeter = 6.28*radius_; 
+    return Perimeter;   
+}
+
+double getArea() {
+    double Area = 3.14*(radius_^2);
+    return Area;
 }
 
 private:
@@ -28,15 +39,45 @@ int radius_ = 0;
 
 class RTriangle : public Shapes {
 public:
+RTriangle(int length, int width){
+    length_ = length;
+    width_ = width;
+}
+
+double getPerimeter(){
+    double Perimeter = length_ + width_ + (sqrt((length_^2) +(width_^2)));
+    return Perimeter;
+}
+
+double getArea(){
+    double Area = .5*length_*width_;
+    return Area;
+}
 
 private:
-
+int length_ = 0;
+int width_ = 0;
 
 };
 
 class Rectangle : public Shapes {
 public:
+Rectangle(int length, int width){
+    length_ = length;
+    width_ = width;
+}
+
+double getPerimeter(){
+    double Perimeter = (2*length_) + (2*width_);
+    return Perimeter;
+}
+
+double getArea(){
+    double Area = length_*width_;
+    return Area;
+}
 
 private:
-
+int length_ = 0;
+int width_ = 0;
 };
